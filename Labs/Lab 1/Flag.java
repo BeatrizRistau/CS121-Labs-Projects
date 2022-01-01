f21f8db51a29495e625c2489e65183d7335b5785
Beatriz Ristau <69829870+BeatrizRistau@users.noreply.github.com>
2021-12-31 20:37:09 -0500

    Add files via upload

---
+++ Labs/Lab 1/Flag.java
@@ -0,0 +1,19 @@
+/**
+ * Name: Beatriz Ristau
+ * Course: CS 121
+ * Professor: Dr. Rai
+ * Institution: WVU Tech
+ */
+
+public class Flag {
+  public static void  main (String [] args) {
+    
+    System.out.println ("**********===============");
+    System.out.println ("**********===============");
+    System.out.println ("**********===============");
+    System.out.println ("**********===============");
+    System.out.println ("=========================");
+    System.out.println ("=========================");
+    System.out.println ("=========================");
+  }
+}
---
+++ Labs/Lab 1/Initials.java
@@ -0,0 +1,19 @@
+/**
+ * Name: Beatriz Ristau
+ * Course: CS 121
+ * Professor: Dr. Rai
+ * Institution: WVU Tech
+ */
+
+public class Initials {
+  public static void main (String [] args) {
+    
+    System.out.println ("BBBBBBBB  RRRRRRRR");
+    System.out.println ("BB    BB  RR    RR");
+    System.out.println ("BB    BB  RR    RR");
+    System.out.println ("BBBBBBBB  RRRRRRRR");
+    System.out.println ("BB    BB  RR     RR");
+    System.out.println ("BB    BB  RR      RR");
+    System.out.println ("BBBBBBBB  RR       RR");
+  }
+}
